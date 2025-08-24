3478179f4a9ead5a9b0144b140d64428f8f1b081
leonaiyem <mdnaiyem.islam.10@gmail.com>
2025-08-24 22:27:47 +0600

    forgot email problem resovled

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -16,5 +16,7 @@ return [
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'forgot_password_message' => 'Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.',
+    'email_password_reset_link' => 'Email Password Reset Link',
 
 ];
